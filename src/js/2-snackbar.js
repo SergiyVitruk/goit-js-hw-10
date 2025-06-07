830a2b3c476f0bae99501ce5845992772f5416ab
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-06-07 20:15:54 +0300

    finish 2 tasks

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import '../css/2-snackbar.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const { delay, state } = event.target.elements;
+  const delayValue = +delay.value;
+  const stateValue = state.value;
+
+  setTimeout(() => {
+    new Promise((resolve, reject) => {
+      if (stateValue === 'fulfilled') {
+        resolve(delayValue);
+      } else {
+        reject(delayValue);
+      }
+    })
+      .then(data =>
+        iziToast.success({
+          title: 'OK',
+          message: `✅ Fulfilled promise in ${data}ms`,
+          position: 'topRight',
+        })
+      )
+      .catch(data =>
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Rejected promise in ${data}ms`,
+          position: 'topRight',
+        })
+      );
+  }, delayValue);
+}
